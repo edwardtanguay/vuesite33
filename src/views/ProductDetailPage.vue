2f0573761c570f851237cd8f22a3f841f1c1d33d
Edward Tanguay (wk) <edward@tanguay.info>
2020-10-30 15:07:02 +0100

    add product and cart pages

---
+++ src/views/ProductDetailPage.vue
@@ -0,0 +1,15 @@
+<template>
+	<h1>Product Detail Page</h1>
+</template>
+
+<script>
+export default {
+	name: 'ProductDetailPage'
+}
+</script>
+
+<style>
+h1 {
+	color: #999;
+}
+</style>
---
+++ src/views/ProductsPage.vue
@@ -0,0 +1,15 @@
+<template>
+	<h1>Products Page</h1>
+</template>
+
+<script>
+export default {
+	name: 'ProductsPage'
+}
+</script>
+
+<style>
+h1 {
+	color: #999;
+}
+</style>
